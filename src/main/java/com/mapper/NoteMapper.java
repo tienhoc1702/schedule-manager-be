7f3747073a72d2b5523923824d3dc1de538aa837
ngothiducnhu <ngothiducnhu30052003@gmail.com>
2024-08-11 17:35:53 +0700

    add location in entity Note

--- src/main/java/com/mapper/NoteMapper.java
+++ src/main/java/com/mapper/NoteMapper.java
@@ -5,22 +5,21 @@ import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
-
-import com.dto.AreaDTO;
 import com.dto.NoteDTO;
-
 import com.entity.Note;
 
-@Mapper(componentModel = "spring")
+@Mapper(uses = {NoteTypeMapper.class, StudentMapper.class})
 public interface NoteMapper {
 
 	NoteMapper INSTANCE = Mappers.getMapper(NoteMapper.class);  
 
-	@Mapping(source = "student.id", target = "student.id")
+	@Mapping(source = "student.id", target = "student_id")
+	@Mapping(source = "student.code", target = "student_code")
     @Mapping(source = "noteType.id", target = "notetype_id")
     NoteDTO toDTO(Note note);  
     
-    @Mapping(source = "student.id", target = "student.id")
+    @Mapping(source = "student_id", target = "student.id")
+    @Mapping(source = "student_code", target = "student.code")
     @Mapping(source = "notetype_id", target = "noteType.id")
     Note toEntity(NoteDTO noteDTO);
     
--- src/main/java/com/repository/NoteDAO.java
+++ src/main/java/com/repository/NoteDAO.java
@@ -1,37 +1,39 @@
 package com.repository;
 
-
-import java.util.Date;
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
-
+import com.dto.NotesCountDTO;
 import com.entity.Note;
-@Repository
+
 public interface NoteDAO extends JpaRepository<Note, Integer> {
+	
+	//Find notes by date
 	@Query("SELECT n " +
 		       "FROM Note n " +
 		       "JOIN n.student s " +
 		       "WHERE s.code = :code " +
+		       "AND FUNCTION('DAY', n.date) = :day " +
 		       "AND FUNCTION('MONTH', n.date) = :month " +
 		       "AND FUNCTION('YEAR', n.date) = :year")
-		List<Note> findNotesByStudentCodeAndMonthAndYear(
+	List<Note> findNotesByDate(
 		    @Param("code") String code,
+		    @Param("day") Integer day,
 		    @Param("month") Integer month,
 		    @Param("year") Integer year);
-	
-	@Query("SELECT n " +
-		       "FROM Note n " +
-		       "JOIN n.student s " +
-		       "WHERE s.code = :code " +
-		       "AND n.date = :date")
-		List<Note> findNotesByStudentCodeAndDate(
-		    @Param("code") String code,
-		    @Param("date") Date date);
 
+	 //Count notes
+	 @Query("SELECT new com.dto.NotesCountDTO(" +
+		       "SUM(CASE WHEN nt.id = 1 THEN 1 ELSE 0 END)," +
+		       "SUM(CASE WHEN nt.id = 2 THEN 1 ELSE 0 END))" +
+		       "FROM Note n " +
+		       "JOIN n.noteType nt " +
+		       "JOIN n.student s " +
+		       "WHERE FUNCTION('MONTH', n.date) = :month " +
+		       "AND s.code = :code " +
+		       "GROUP BY FUNCTION('MONTH', n.date) " +
+		       "ORDER BY FUNCTION('MONTH', n.date)")
+		NotesCountDTO findNotesByMonthAndStudentCode(@Param("month") int month, @Param("code") String code);
 
 }
